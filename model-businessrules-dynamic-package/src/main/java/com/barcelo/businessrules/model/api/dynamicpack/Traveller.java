66169cbbdd6fa03f045c81a9867312f59ed2bc24
David <dag-vsf@users.noreply.github.com>
2015-02-19 17:24:56 +0100

    Eliminar imports no usados.

--- model-businessrules-dynamic-package/src/main/java/com/barcelo/businessrules/model/api/dynamicpack/Traveller.java
+++ model-businessrules-dynamic-package/src/main/java/com/barcelo/businessrules/model/api/dynamicpack/Traveller.java
@@ -1,8 +1,6 @@
 package com.barcelo.businessrules.model.api.dynamicpack;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 /**
  * Class that represents each of the travellers that are going to make use of the DynamicPackage.
